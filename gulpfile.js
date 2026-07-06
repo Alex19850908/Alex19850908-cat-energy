9256c6cf7f424a6142e7219de05101fb23f36f53
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-07-06 19:43:51 +0300

    Исправляет проблему сборки

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,8 @@ export function processStyles () {
     .pipe(server.stream());
 }
 
-export function processScripts () {
+// Здесь забыл async
+export async function processScripts () {
   const gulpEsbuild = createGulpEsbuild({ incremental: isDevelopment });
 
   return src(`${PATH_TO_SOURCE}scripts/*.js`)
